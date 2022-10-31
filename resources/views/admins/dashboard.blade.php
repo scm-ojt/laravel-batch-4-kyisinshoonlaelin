089abab25bda62f69b9f1af9713e3f3345b2c332
KyiSin-SCM01~ <scm.kyisinshoonlaelinn@gmail.com>
2022-10-31 13:59:12 +0630

    remove unnecessary js

--- resources/views/admins/dashboard.blade.php
+++ resources/views/admins/dashboard.blade.php
@@ -172,12 +172,9 @@
 
     <!-- jQuery -->
     <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
     <!-- AdminLTE App -->
     <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
-    <!-- jQuery -->
-    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/js/jquery-2.2.4.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/js/jquery.multi-select.js') }}"></script>
     <script type="text/javascript">
